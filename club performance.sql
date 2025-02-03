f406ad86b1296a1713ff5ef2ca2614b9c5fa48f9
sulemanbasit <83623379+sulemanbasit@users.noreply.github.com>
2025-02-03 02:01:02 -0700

    Update club performance.sql

--- club performance.sql
+++ club performance.sql
@@ -85,3 +85,61 @@ GROUP BY GROUPING SETS ((club), ());*/
     END AS total_matches
 FROM "UCL".disciplinary
 GROUP BY GROUPING SETS ((club), ()); */
+
+/* Script to get club's distribution stats */
+
+/* SELECT 
+    CASE 
+        WHEN GROUPING(club) = 1 THEN 'Total' -- Label for total row
+        ELSE club
+    END AS club,
+     -- Calculate pass accuracy based on completed/attempted passes
+    ROUND((SUM(pass_completed) * 100.0) / NULLIF(SUM(pass_attempted), 0), 2) AS avg_pass_accuracy,
+    SUM(pass_attempted) AS total_pass_attempted,
+    SUM(pass_completed) AS total_pass_completed,
+    -- Calculate cross accuracy the same way
+    ROUND((SUM(cross_complted) * 100.0) / NULLIF(SUM(cross_attempted), 0), 2) AS avg_cross_accuracy,
+    sum(cross_attempted) as total_cross_attempted,
+    sum(cross_complted) as total_cross_completed,
+    sum(freekicks_taken) as total_freekicks_taken,
+    CASE 
+        WHEN GROUPING(club) = 1 THEN (
+            SELECT SUM(max_matches)
+            FROM (
+                SELECT club, MAX(match_played) AS max_matches
+                FROM "UCL".distributon
+                GROUP BY club
+            ) max_matches_per_club
+        )
+        ELSE MAX(match_played) -- For individual rows, show max matches played
+    END AS total_matches
+FROM "UCL".distributon
+GROUP BY GROUPING SETS ((club), ()); */
+
+-- Club's goal stats
+select 
+	case 
+		when grouping(club) = 1 then 'Total'
+		else club
+	end as club,
+	sum(goals) as total_goals,
+	sum(right_foot) as total_right_foot_goals,
+	sum(left_foot) as total_left_foot_goals,
+	sum(headers) as total_header_goals,
+	sum(others) as total_other_goals,
+	sum(penalties) as total_penalties,
+	sum(inside_area) as total_inside_goals,
+	sum(outside_areas) as total_outside_goals,
+	case 
+		WHEN GROUPING(club) = 1 THEN (
+            SELECT SUM(max_matches)
+            FROM (
+                SELECT club, MAX(match_played) AS max_matches
+                FROM "UCL".distributon
+                GROUP BY club
+            ) max_matches_per_club
+        )
+        ELSE MAX(match_played) -- For individual rows, show max matches played
+    END AS total_matches
+	from "UCL".goals
+	group by grouping sets ((club), ());
