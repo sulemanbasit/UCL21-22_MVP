be1eb7c9dc3995f97546ba39c30999f1c99f6f66
sulemanbasit <83623379+sulemanbasit@users.noreply.github.com>
2025-02-19 10:53:36 -0700

    added a P.K before joining and adding more columns for more accurate analysis

--- player_stats.sql
+++ player_stats.sql
@@ -6,58 +6,96 @@
 --)
 --SELECT COUNT(*) FROM selected_data;
 
+--SELECT column_name
+--FROM information_schema.columns
+--WHERE table_name = 'key_stats' AND column_name = 'unique_id';
 
---create view final_data as
---with final_data as (
---select ks.*, 
---	att.corner_taken ,
---	att.offsides  , 
---	att.dribbles , 
---	d.tackles , 
---	d.t_won , 
---	d.t_lost , 
---	d.balls_recoverd , 
---	d.clearance_attempted,
---	a.total_attempts ,
---	a.on_target ,
---	a.off_target ,
---	a."blocked" ,
---	d2.red ,
---	d2.yellow ,
---	d2.fouls_committed ,
---	d2.fouls_suffered ,
---	d3.cross_accuracy ,
---	d3.cross_attempted ,
---	d3.cross_complted ,
---	d3.freekicks_taken ,
---	d3.pass_accuracy ,
---	d3.pass_attempted ,
---	d3.pass_completed ,
---	g.right_foot ,
---	g.left_foot ,
---	g.headers ,
---	g.penalties ,
---	g."others" ,
---	g.inside_area ,
---	g.outside_areas ,
---	g2.saved ,
---	g2.saved_penalties ,
---	g2.conceded ,
---	g2.cleansheets ,
---	g2."punches made" 
---from key_stats ks
---left join attacking att on ks.player_name = att.player_name
---left join defending d on ks.player_name = d.player_name 
---left join attempts a on ks.player_name = a.player_name 
---left join disciplinary d2 on ks.player_name = d2.player_name 
---left join distributon d3 on ks.player_name = d3.player_name 
---left join goals g on ks.player_name = g.player_name 
---left join goalkeeping g2 on ks.player_name = g2.player_name 
---)
---select * from final_data ;
+--ALTER TABLE key_stats ADD COLUMN unique_id VARCHAR(255);
+----ALTER TABLE attacking ADD COLUMN unique_id VARCHAR(255);
+----ALTER TABLE defending ADD COLUMN unique_id VARCHAR(255);
+--ALTER TABLE attempts ADD COLUMN unique_id VARCHAR(255);
+--ALTER TABLE disciplinary ADD COLUMN unique_id VARCHAR(255);
+--ALTER TABLE distributon ADD COLUMN unique_id VARCHAR(255);
+----ALTER TABLE goals ADD COLUMN unique_id VARCHAR(255);
+--ALTER TABLE goalkeeping ADD COLUMN unique_id VARCHAR(255);
+--
+--UPDATE key_stats 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE attacking 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE defending 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE attempts 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE disciplinary 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE distributon 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE goals 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
+--UPDATE goalkeeping 
+--SET unique_id = CONCAT(player_name, '_', position, '_', club)
+--WHERE unique_id IS NULL OR unique_id = '';
 
---SELECT player_name, club, COUNT(*) AS duplicates
---FROM final_data
---GROUP BY player_name, club
---HAVING COUNT(*) > 1
---ORDER BY player_name;
+--drop view correct_final_data ;
+CREATE VIEW correct_final_data AS
+WITH correct_final_data AS (
+    SELECT ks.*, 
+        att.corner_taken ,
+        att.offsides  , 
+        att.dribbles , 
+        d.tackles , 
+        d.t_won , 
+        d.t_lost , 
+        d.balls_recoverd , 
+        d.clearance_attempted,
+        a.total_attempts ,
+        a.on_target ,
+        a.off_target ,
+        a."blocked" ,
+        d2.red ,
+        d2.yellow ,
+        d2.fouls_committed ,
+        d2.fouls_suffered ,
+        d3.cross_accuracy ,
+        d3.cross_attempted ,
+        d3.cross_complted ,
+        d3.freekicks_taken ,
+        d3.pass_accuracy ,
+        d3.pass_attempted ,
+        d3.pass_completed ,
+        g.right_foot ,
+        g.left_foot ,
+        g.headers ,
+        g.penalties ,
+        g."others" ,
+        g.inside_area ,
+        g.outside_areas ,
+        g2.saved ,
+        g2.saved_penalties ,
+        g2.conceded ,
+        g2.cleansheets ,
+        g2."punches made" 
+    FROM key_stats ks
+    LEFT JOIN attacking att ON ks.unique_id = att.unique_id
+    LEFT JOIN defending d ON ks.unique_id = d.unique_id 
+    LEFT JOIN attempts a ON ks.unique_id = a.unique_id 
+    LEFT JOIN disciplinary d2 ON ks.unique_id = d2.unique_id 
+    LEFT JOIN distributon d3 ON ks.unique_id = d3.unique_id 
+    LEFT JOIN goals g ON ks.unique_id = g.unique_id 
+    LEFT JOIN goalkeeping g2 ON ks.unique_id = g2.unique_id 
+)
+SELECT * FROM correct_final_data;
+
+SELECT unique_id, COUNT(*) AS duplicates
+FROM correct_final_data
+GROUP BY unique_id
+HAVING COUNT(*) > 1
+ORDER BY unique_id;
